932bfa182442b81d913b9c7980079a8e75532961
Rémy Picciano <remypicciano@icloud.com>
2025-06-25 11:47:12 -0400

    Hopefully google analytics fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import GoogleTag from './components/GoogleTag';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import ContactForm from './components/ContactForm';
@@ -9,24 +10,27 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Header />
-      <main>
-        <Hero />
-        
-        {/* Contact Form Section */}
-        <section id="contact" className="py-16 lg:py-24 bg-gradient-to-br from-blue-50 to-blue-100">
-          <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-            <ContactForm />
-          </div>
-        </section>
+    <>
+      <GoogleTag />
+      <div className="min-h-screen">
+        <Header />
+        <main>
+          <Hero />
+          
+          {/* Contact Form Section */}
+          <section id="contact" className="py-16 lg:py-24 bg-gradient-to-br from-blue-50 to-blue-100">
+            <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+              <ContactForm />
+            </div>
+          </section>
 
-        <About />
-        <Services />
-        <Testimonials />
-      </main>
-      <Footer />
-    </div>
+          <About />
+          <Services />
+          <Testimonials />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
 
